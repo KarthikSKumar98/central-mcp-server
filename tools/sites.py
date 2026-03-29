3ca997f5ceb308fdd2a459bf55858f9f95b8b6f5
KarthikSKumar98 <karthiksk98@gmail.com>
2026-03-30 00:23:55 +0400

    Revert "Merge pull request #19 from KarthikSKumar98/site-summary-bug-fix"
    This reverts commit 985228f225a9e1fb119826a69383545ad083c74a, reversing
    changes made to f1afcdd9ad91a6a8a2a687e03f30385ab5c9f360.

--- tools/sites.py
+++ tools/sites.py
@@ -61,8 +61,8 @@ def register(mcp: FastMCP) -> None:
             mapping[site["siteName"]] = {
                 "site_id": site.get("id"),
                 "health": summary,
-                "total_devices": site.get("devices", {}).get("count", 0),
-                "total_clients": site.get("clients", {}).get("count", 0),
+                "total_devices": site.get("devices", {}).get("total", 0),
+                "total_clients": site.get("clients", {}).get("total", 0),
                 "total_alerts": site.get("alerts", {}).get("total", 0),
             }
         mapping = dict(
