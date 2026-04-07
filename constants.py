9d3fa756ceb90379d167c32d017fc69d395d43c0
KarthikSKumar98 <karthiksk98@gmail.com>
2026-04-07 21:59:21 +0400

    feat: add WLAN_LIMIT constant for API call limits

--- constants.py
+++ constants.py
@@ -3,6 +3,7 @@ from typing import Literal
 SITE_LIMIT = 100  # Max number of sites returned per API call
 ALERT_LIMIT = 50  # Max number of alerts returned per API call
 EVENT_LIMIT = 50  # Max number of events returned per API call
+WLAN_LIMIT = 100  # Max number of WLANs returned per API call
 API_CONCURRENCY_LIMIT = 5  # Max concurrent outbound Central API calls
 
 TIME_RANGE = Literal[
